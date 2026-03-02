f4426a60216031420a4a5369ccabe168c83d130b
zs <zhangsong325@gmail.com>
2026-03-02 23:20:31 +0800

    simplify cmake targets

---
+++ include/outcome/ext/exception.hpp
@@ -0,0 +1,7 @@
+#ifndef OUTCOME_LIBRARY_WRAPPER_EXT_EXCEPTION_HPP
+#define OUTCOME_LIBRARY_WRAPPER_EXT_EXCEPTION_HPP
+
+#include <outcome/vendor/outcome-experimental.hpp>
+#include <outcome/vendor/system_code_from_exception.hpp>
+
+#endif // OUTCOME_LIBRARY_WRAPPER_EXT_EXCEPTION_HPP
---
+++ include/outcome/ext/std_error_code.hpp
@@ -0,0 +1,7 @@
+#ifndef OUTCOME_LIBRARY_WRAPPER_EXT_STD_ERROR_CODE_HPP
+#define OUTCOME_LIBRARY_WRAPPER_EXT_STD_ERROR_CODE_HPP
+
+#include <outcome/vendor/outcome-experimental.hpp>
+#include <outcome/vendor/std_error_code.hpp>
+
+#endif // OUTCOME_LIBRARY_WRAPPER_EXT_STD_ERROR_CODE_HPP
